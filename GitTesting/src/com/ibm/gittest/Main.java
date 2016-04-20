a476d28050c63edc8dba0e7a827771a172633da1
kandu <kandu@oc2756228212>
2016-04-20 14:59:55 -0400

    create a conflict

--- GitTesting/src/com/ibm/gittest/Main.java
+++ GitTesting/src/com/ibm/gittest/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-		System.out.println("somework done in kandu-new-branch"); 
+		System.out.println("somework done in kandu-new-branch"); //create a conflict
 	}
 	public static void somefunction()
 	{
