55ecbfaef42a7ec0af5346a8b66c0a2fa05c28d2
kandu <kandu@oc2756228212>
2016-04-20 14:34:06 -0400

    reverted back to the very beginning

--- GitTesting/src/com/ibm/gittest/Main.java
+++ GitTesting/src/com/ibm/gittest/Main.java
@@ -4,9 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//Added something
-		//added something again by someone
-		//added something by someone again
+
 	}
 
 }
