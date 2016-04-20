ff4cbe534725ffdd52c69396c77b93b607370078
kandu <kandu@oc2756228212>
2016-04-20 14:43:32 -0400

    kandu-new-branch code

--- GitTesting/src/com/ibm/gittest/Main.java
+++ GitTesting/src/com/ibm/gittest/Main.java
@@ -2,9 +2,10 @@ package com.ibm.gittest;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
-
+		System.out.println("somework done in kandu-new-branch"); 
 	}
 
 }
