5c5286fb48e7a95a730cce74afc5cef81bc062b1
kanduibm <kandu@ca.ibm.com>
2016-04-20 15:00:30 -0400

    Update Main.java
    another user edit it the same line

--- GitTesting/src/com/ibm/gittest/Main.java
+++ GitTesting/src/com/ibm/gittest/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-		System.out.println("somework done in kandu-new-branch"); 
+		System.out.println("somework done in kandu-new-branch"); //another conflict by some user from web
 	}
 	public static void somefunction()
 	{
